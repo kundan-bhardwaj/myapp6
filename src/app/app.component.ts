12fda56320f0bac6d3a9238dcc268b44ffafd779
Kundan Bhardwaj <kundanbhardwaj145@gmail.com>
2021-06-29 22:22:16 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp6';
+    title = 'myapp6';
+    show() {
+        let a = < HTMLDivElement > document.getElementById("sidebar");
+        let c = a.style.visibility;
+        if (c == "hidden") {
+            a.style.cssText = "visibility : visible;animation: 0.3s zoomIn";
+        } else {
+            a.style.cssText = "animation: 1s bounceOutUp;visibility : hidden";
+        }
+    }
 }
